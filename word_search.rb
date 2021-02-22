7d65fa94199cdb4a24fdadd01d5500298299b13a
ChakravarthiDinavahi <chakravarthi.dinavahi@gmail.com>
2021-02-22 11:12:17 +0530

    Update word_search.rb
    Divided operations into methods

--- word_search.rb
+++ word_search.rb
@@ -2,38 +2,55 @@ class WordSearch
   def initialize(board, word)
     @word = word.to_s.split('')
     @board = board
+    @search = {}
   end
 
   def process
-    search = {}
     @word.each do |letter|
-      r = []
       index1 = @board.index letter
       index2 = @board.reverse.index letter
       if index1.present? && index2.present? && index1 <= index2
-        @board.each do |_l|
-          k = @board.shift
-          break if letter == k
-
-          @board << k
-
-          r << 'LEFT'
-          search[letter] = r
-        end
+        left_operation(letter)
       else
-        @board.each do |_l|
-          k = @board.pop
-          break if letter == k
-
-          @board.unshift(k)
-
-          r << 'RIGHT'
-          search[letter] = r
-        end
+        right_operation(letter)
       end
     end
-    search.each do |k, v|
-      puts v.join(', ') + ' :' + k
+    print_output
+  end
+
+  def left_operation(letter)
+    r = []
+    r << 'LEFT'
+
+    @board.each do |_l|
+      k = @board.shift
+      break if letter == k
+
+      @board << k
+
+      r << 'LEFT'
+      @search[letter] = r
+    end
+  end
+
+  def right_operation(letter)
+    r = []
+    r << 'RIGHT'
+
+    @board.each do |_l|
+      k = @board.pop
+      break if letter == k
+
+      @board.unshift(k)
+
+      r << 'RIGHT'
+      @search[letter] = r
+    end
+  end
+
+  def print_output
+    @search.each do |k, v|
+      puts v.join(', ') + ':' + k
     end
   end
 end
