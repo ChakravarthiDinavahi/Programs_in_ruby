96f137d3e9a32c1884b9ef631f93c479f600facf
ChakravarthiDinavahi <chakravarthi.dinavahi@gmail.com>
2021-04-16 16:18:37 +0530

    updated with printing of traversals

--- binary_search_tree.rb
+++ binary_search_tree.rb
@@ -10,54 +10,57 @@ class Node
   end
 end
 
-
 class BinarySearchTree
   def initialize
-  @n1 = nil
+    @root = nil
+    constuct
+    traversals
   end
 
-  def self.constuct
-    @n1 = Node.new(1)
-    @n1.left =Node.new(2)
-    @n1.right = Node.new(3)
-    @n1.left.left = Node.new(4)
-    @n1.left.right = Node.new 5
+  def constuct
+    puts "tree with 5 nodes construction started"
+    @root = Node.new(1)
+    @root.left =Node.new(2)
+    @root.right = Node.new(3)
+    @root.left.left = Node.new(4)
+    @root.left.right = Node.new 5
+    puts "tree with 5 nodes construction done"
   end
 
-  def self.pre_order(node)
+  def traversals
+    preorder = preorder(@root)
+    puts "Preorder traversal of trees is #{preorder.join(",")}"
+    inorder = inorder(@root)
+    puts "Inorder traversal of trees is #{inorder.join(",")}"
+    postorder = postorder(@root)
+    puts "Postorder traversal of trees is #{postorder.join(",")}"
+  end
+
+  def preorder(node)
     #Root, Left and Right
     if node.left == nil || node.right == nil
      return node.value
     end
-    return [node.value,pre_order(node.left),pre_order(node.right)].flatten
+    return [node.value,preorder(node.left),preorder(node.right)].flatten
   end
 
 
-  def self.post_order(node)
+  def postorder(node)
     #Left and Right, root
     if node.left == nil || node.right == nil
       return node.value
      end
-     return [post_order(node.left),post_order(node.right),node.value].flatten
- 
+     return [postorder(node.left),postorder(node.right),node.value].flatten
   end
 
-  def self.inorder_order(node)
+  def inorder(node)
     #Left, root and Right
     if node.left == nil || node.right == nil
       return node.value
     end
-     return [inorder_order(node.left),node.value,inorder_order(node.right)].flatten
+     return [inorder(node.left),node.value,inorder(node.right)].flatten
   end
 
 end
-# BST construction
-n1 = Node.new(1)
-n1.left=Node.new(2)
-n1.right=Node.new(3)
-n1.left.left = Node.new(4)
-n1.left.right =Node.new 5
 
-BinarySearchTree.pre_order(n1)
-BinarySearchTree.post_order(n1)
-BinarySearchTree.inorder_order(n1)
+BinarySearchTree.new
